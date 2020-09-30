31db2cb3e6fa7844aa63ee4c487bc9881fe8dc3e
wanguan <1326075334@qq.com>
2020-09-30 23:26:02 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,23 @@
-const path = require('path')
+const CompressionPlugin = require('compression-webpack-plugin')
+const path = require("path");
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+  return path.join(__dirname, dir);
 }
 
 // 项目的主要配置文件
 module.exports = {
-  chainWebpack: (config) => {
+  chainWebpack: config => {
     // 修改文件引入自定义路径
     config.resolve.alias
-      .set('@', resolve('src'))
-      .set('views', resolve('src/views'))
-      .set('assets', resolve('src/assets'))
-      .set('components', resolve('src/components'))
-      .set('network', resolve('src/network'))
-      .set('common', resolve('src/common'))
+      .set("@", resolve("src"))
+      .set("views", resolve("src/views"))
+      .set("assets", resolve("src/assets"))
+      .set("components", resolve("src/components"))
+      .set("network", resolve("src/network"))
+      .set("common", resolve("src/common"));
   },
   configureWebpack: {
     // devtool: 'source-map'
   },
 }
-
-
-
