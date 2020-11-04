b6a30735567cdbc95fd54dd3926c48bdefea10db
wanguan <1326075334@qq.com>
2020-11-04 08:58:35 +0800

    优化用户体验: 添加了骨架屏

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-// const { SkeletonPlugin } = require('page-skeleton-webpack-plugin')
+const { SkeletonPlugin } = require('page-skeleton-webpack-plugin')
 const path = require("path");
 
 function resolve(dir) {
@@ -7,15 +7,15 @@ function resolve(dir) {
 
 // 项目的主要配置文件
 module.exports = {
-  // configureWebpack: {
-  //   plugins: [
-  //     new SkeletonPlugin({
-  //       pathname: path.resolve(__dirname, './shell'), // 用来存储 shell 文件的地址
-  //       staticDir: path.resolve(__dirname, './dist'), // 最好和 `output.path` 相同
-  //       routes: ['/','/about'], // 将需要生成骨架屏的路由添加到数组中
-  //     })
-  //   ],
-  // },
+  configureWebpack: {
+    plugins: [
+      new SkeletonPlugin({
+        pathname: path.resolve(__dirname, './shell'), // 用来存储 shell 文件的地址
+        staticDir: path.resolve(__dirname, './dist'), // 最好和 `output.path` 相同
+        routes: ['/'], // 将需要生成骨架屏的路由添加到数组中
+      })
+    ],
+  },
   chainWebpack: config => {
     // 修改文件引入自定义路径
     config.resolve.alias
@@ -27,11 +27,11 @@ module.exports = {
       .set("common", resolve("src/common"));
 
       // 骨架屏
-      // if (process.env.NODE_ENV !== 'development') {
-      //   config.plugin('html').tap(opts => {
-      //     opts[0].minify.removeComments = false
-      //     return opts
-      //   })
-      // }
+      if (process.env.NODE_ENV !== 'development') {
+        config.plugin('html').tap(opts => {
+          opts[0].minify.removeComments = false
+          return opts
+        })
+      }
   },
 };
